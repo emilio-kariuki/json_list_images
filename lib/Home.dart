1371ab9de149d8f8dfc2595a408d1cf5330edb41
emilio kariuki <70817006+emilio-kariuki@users.noreply.github.com>
2022-02-05 19:55:32 +0300

    February 5, 2022 7:55 PM

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,10 @@
-// ignore_for_file: await_only_futures
+// ignore_for_file: await_only_futures, unused_label
+import 'dart:convert';
 
 import "package:flutter/material.dart";
-import "package:json_list_images/Service.dart";
+import 'package:flutter/services.dart' as rootBundle;
+import "package:json_list_images/User.dart";
+import "package:http/http.dart" as http;
 
 import 'User.dart';
 
@@ -13,32 +16,51 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Future<List<User>> getUser() async => Service.getUsers().then((user) {
-        List<User> _users = user;
-        return _users;
-      });
-
-  @override
-  void initState() {
-    super.initState();
-    getUser();
+  // ignore: non_constant_identifier_names
+  Future<List<User>> ReadData() async {
+    try {
+      final response = await http
+          .get(Uri.parse("https://jsonplaceholder.typicode.com/photos"));
+      if (response.statusCode == 200) {
+        final List<User> user = userFromJson(response.body);
+        return user;
+      }else{
+        return <User>[];
+      }
+    } catch (e) {
+      return <User>[];
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final mediaQuery = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: ListView.builder(
-              itemBuilder: ((context, index) {
-                User user = _users[index];
-                return FittedBox(
-                  child: Image.network(user.url),
-                  fit: BoxFit.fill,
-                );
-              }),
-            )));
+      body: FutureBuilder(
+          future: ReadData(),
+          builder: (context, data) {
+            if (data.hasError) {
+              return Center(
+                child: const Text("The data has an error"),
+              );
+            } else if (data.hasData) {
+              var items = data.data as List<User>;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  return Card(
+                    elevation: 5,
+                    margin: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                    child: Container(
+                      child: Image.network(items[index].url.toString(),fit: BoxFit.cover,),
+                    )
+                  );
+                },
+                itemCount: items.length,
+              );
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }),
+    );
   }
 }
